4acefbad96d38ca41eba8aabc12d3d1c873641cd
JD-Fuller <jdfuller@live.com>
2020-02-28 16:25:15 -0700

    not quite working

--- index.py
+++ index.py
@@ -1,11 +1,49 @@
-msg = "This is a great Hello World kind of day"
+# msg = "This is a great Hello World kind of day"
 
-print(msg)
+# print(msg)
 
-print(msg + '5')
-arrayLarge = []
-print(arrayLarge)
+# print(msg + '5')
+# arrayLarge = []
+# print(arrayLarge)
 
-arrayLarge.append(5)
+# arrayLarge.append(5)
 
-print(arrayLarge)
+# print(arrayLarge)
+
+
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = raw_input("Ask the magic 8 ball a question:")
+
+    answers = random.randit(1, 8)
+
+    if question == ''
+    sys.exit()
+
+    elif answers == 1
+    print "it is certain"
+
+    elif answers == 2
+    print "outlook good"
+
+    elif answers == 3
+    print "You may rely on it"
+
+    elif answers == 4
+    print "Ask again later"
+
+    elif answers == 5
+    print "Concentrate and ask again"
+
+    elif answers == 6
+    print "Not today!"
+
+    elif answers == 7
+    print "Yeah, right."
+
+    elif answers == 8
+    print "No way jose"
